20101445a644bbeb527377e397fb3c8a37e43e93
camiloYateY2018 <cyateya35439@universidadean.edu.co>
2019-08-29 00:53:39 -0500

    first commit2

--- isomatic/settings.py
+++ isomatic/settings.py
@@ -31,11 +31,11 @@ ALLOWED_HOSTS = []
 
 
 # TODO CONFIGURAR ENVIO DE EMAIL
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'solpromat@gmail.com'
-EMAIL_HOST_PASSWORD = 'Visa1234556'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'solpromat@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Visa1234556'
+# EMAIL_PORT = '587'
+# EMAIL_USE_TLS = True
 # TODO CONFIGURAR ENVIO DE EMAIL
 
 # Application definition
